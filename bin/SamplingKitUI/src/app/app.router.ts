b148201010eb233b663002df7bfe2a22ff2a2cb1
Anilkumar Marri <manilkumar@corp.ojas-it.com>
2019-05-22 13:09:21 +0530

    changed EthniC Group resource

--- bin/SamplingKitUI/src/app/app.router.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { Indexroutes } from './index/index.router';
-import { HomeRoutes } from './home/home.router';
-import { NoPageRoute } from './no-page/no-page.router';
-
-export const routes: Routes = [...HomeRoutes, ...Indexroutes, ...NoPageRoute]
